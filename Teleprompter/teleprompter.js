5b3e8863c76fd977ec04a2085136ece596fc0795
Darren Xulouis <12116493@student.uts.edu.au>
2017-10-26 01:05:11 +1100

    finished initial development
    further tweaks might be needed to make the code beter. Core
    functionality done

--- Teleprompter/teleprompter.js
+++ Teleprompter/teleprompter.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-
-});
-
 $(".button").mouseenter(function(){
 		$("#textarea").css("box-shadow","5px 5px 1px #888888");
 	});
@@ -13,15 +9,32 @@ $(".button").mouseleave(function(){
 $("#teleprompter").submit(function(e){
 	e.preventDefault();
 	var content = $("#teleprompter > textarea").val();
+	var startHeight = 0;
+
+	if (content.replace(/^\s+|\s+$/g, '') == '') {
+			content = 'Aww.. no need to be shy, enter some text ;)';
+		}
+
 	$(".main_content").remove();
-	$("body").css("background-color","red");
+	$("body").css("background-color","#000d1a");
 
 	var teletv = $("<div/>");
 	teletv.attr("class","tele_screen");
 	teletv.text(content);
 	$("body").append(teletv);
+
+	teleprompt(startHeight);
 });
 
-function teleprompt(content){
-	
+function teleprompt(y){
+	setTimeout(function(){
+		var newY = y;
+		var height = $(".tele_screen").height();
+		if(newY > -1*height - 150)
+		{
+			newY--;
+			$(".tele_screen").css("top",newY);
+			teleprompt(newY);
+		}	
+	},30);	
 }
