eadfb42b8dcb2707978cc4c8fbf11509d85c3c79
Darren Xulouis <12116493@student.uts.edu.au>
2017-11-09 18:35:25 +1100

    Added content on index page
    Finalized styling on about page, added style change for responsive,
    added send mail section. Need to finish send mail function before
    finalizing the whole index page.

--- index.js
+++ index.js
@@ -12,8 +12,30 @@ $(document).ready(function(){
 	}
 
 
-	/*$(".circle").hover(function(e){
-		console.log(e.target.className);
-		$(e.target).animate({'stroke-dashoffset':1000},1000);
-	});*/
+	$(".circle").hover(function(e){
+		$(".circle").stop().animate({'stroke-dashoffset':1000},1000);
+		$(e.target).stop().animate({'stroke-dashoffset':0},1500);
+	});
+
+	$("li").click(function(){
+		var text = about_text(this.id);
+		$('#about_text').animate({left:'-100%'},{duration:1000,complete: function(){
+			$('#about_text').text(text);
+			$('#about_text').animate({left:'0%'},500);
+		}
+	});
+	});
+
+	function about_text(id){
+		switch(id){
+			case 'about_li_one' : return "Hello, how are you?";
+			case 'about_li_two' : return "I'm doing these to improve myself in front end technologies";
+			case 'about_li_three' : return "The projects are inspired from Jennifer Dewalt's 180 websites in 180 days";
+			case 'about_li_four' : return "How you doing??";
+		}
+	}
+
+	function send_email(){
+		emailjs.send("gmail", "template_fCQya2gw", {"from_name":"Alex","email":"Ales@alex.com","message_html":"Hi I got an idea"})
+	}
 });
